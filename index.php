00802af6716089567567c96ead4e1c2c2e3e9859
Richard Damon Ciampa <ciam1324@mlc104.csumb.edu>
2015-09-17 17:08:51 -0700

    Added formatting to the index.php file, added styles to the css sheet, fixed and added header information to index.php, added site icon.

--- index.php
+++ index.php
@@ -1,32 +1,45 @@
 <!DOCTYPE html>
 <html lang="en-US">
-<link href="css/styles.css" rel="stylesheet">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>SilverJack Game</title>
-  <meta name="description" content="">
-  <meta name="author" content="Andrew Richardson, Brandon Saletta, Richard Ciampa, Susan Espinoza">
-  <meta name="viewport" content="width=device-width; initial-scale=1.0">
-  <link rel="shortcut icon" href="/favicon.ico">
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-</head>
+	<head>
+		<meta charset="utf-8">
 
-<body>
-  <div>
-    <header>
-      <h1>Welcome to SilverJack</h1>
-    </header>
-    <nav>
-    </nav>
+		<title>SilverJack Game</title>
 
-    <div>
-      
-    </div>
+		<meta name="description" content="Silver Jack card game for CST336">
 
-    <footer>
-     <p>&copy; Copyright by Andrew Richardson, Brandon Saletta, Richard Ciampa, Susan Espinoza</p>
-    </footer>
-  </div>
-</body>
+		<!-- Web application authors -->
+		<meta name="author" content="Andrew Richardson" />
+		<meta name="author" content="Susan Espinoza" />
+		<meta name="author" content="Richard Ciampa"/>
+		<meta name="author" content="Brandon Saletta" />
+
+		<!-- Style sheets -->
+		<link href="css/styles.css" rel="stylesheet">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<!-- Site favicon and application icon references -->
+		<!-- <link rel="shortcut icon" href="/favicon.ico"> Unused at the moment -->
+		<link rel="icon" type="image/png" href="./img/csumb-logo-white.png" />
+
+	</head>
+
+	<body>
+		<div idid="wrapper">
+			<header>
+				<h1>Welcome to SilverJack</h1>
+			</header>
+			<nav></nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright by Andrew Richardson, Brandon Saletta, Richard Ciampa, Susan Espinoza
+				</p>
+			</footer>
+		</div>
+	</body>
 </html>
